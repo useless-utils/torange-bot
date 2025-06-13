83208f118e675379cb3137325f65bf2e1c8d46ed
Lucas Gabriel <g@11xx.org>
2025-06-12 23:45:37 -0300

    inital test with environment variables

--- Bot.hs
+++ Bot.hs
@@ -1,4 +1,10 @@
 import Reddit.Actions.Post
+import System.Environment
 
 main :: IO ()
-main = putStrLn "Hi hello"
+main = do
+  username <- getEnv "TORANGE_BOT_REDDIT_USERNAME"
+  secret <- getEnv "TORANGE_BOT_REDDIT_SECRET"
+  putStrLn $ "Hi, " <> username
+
+
