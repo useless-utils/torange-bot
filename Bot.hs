aab3b3b308617c2c01a4188c97e8596991c6d0cd
Lucas Gabriel <g@11xx.org>
2025-06-26 06:44:19 -0300

    cli: initial arg parsing
    test with
    
    : cabal run torange-bot -- -u myUser
    
    example output
    
    > Username from CLI: myUser
    > ERROR: No username provided.

--- Bot.hs
+++ Bot.hs
@@ -2,8 +2,19 @@ import Reddit.Actions.Post
 import System.Environment
 import System.Exit
 
+defaultConfig :: Config
+defaultConfig = Config
+  { username = []
+  , secretFile = ".sec"
+  , twoFA = []
+  }
+
 main :: IO ()
 main = do
+  args <- getArgs
+  let conf = parseArgs args defaultConfig
+  putStrLn $ "Username from CLI: " ++ username conf
+
   usernameEnv <- lookupEnv "TORANGE_BOT_REDDIT_USERNAME"
   secretEnv <- lookupEnv "TORANGE_BOT_REDDIT_SECRET"
 
@@ -15,6 +26,24 @@ main = do
 
   putStrLn $ "Hi, " <> username
 
+data Config = Config
+  { username :: String
+  , secretFile :: FilePath  -- NO secret via cli.
+  , twoFA :: String
+  } deriving (Show, Eq)
+
+parseArgs :: [String] -> Config -> Config
+parseArgs args conf =
+  case args of
+    ("--":_)               -> conf
+    ("-u":a:xs)            -> parseArgs xs conf {username = a}
+    ("--username":a:xs)    -> parseArgs xs conf {username = a}
+    ("-s":a:xs)            -> parseArgs xs conf {secretFile = a}
+    ("--secret-file":a:xs) -> parseArgs xs conf {secretFile = a}
+    ("--2fa":a:xs)         -> parseArgs xs conf {twoFA = a}
+    (_:xs)                 -> parseArgs xs conf
+    []                     -> conf
+
 doesSecretHave2FA :: Maybe String -> Bool
 doesSecretHave2FA (Just env) = go env
   where
@@ -23,5 +52,3 @@ doesSecretHave2FA (Just env) = go env
                   _ -> go xs
     go [] = False
 doesSecretHave2FA Nothing = False
-
-
